b105dc021f76d251a480037ad97cc066dcb27a90
hyunjiyang <70367386+hyunjiyang@users.noreply.github.com>
2020-11-22 04:15:04 +0900

    Update HSV_ver.cpp
    이게 맞는지 모르겠군

--- #2 Automatic lane guidance system/HSV_ver.cpp
+++ #2 Automatic lane guidance system/HSV_ver.cpp
@@ -40,22 +40,46 @@ int main(int argc, char** argv)
 		namedWindow("Original image");
 		imshow("Original image", image);
 
-		cvtColor(image, image, COLOR_BGR2HSV);
-
 		x = image.size().width / 4;
 		y = image.size().height / 2;
 		w = image.size().width / 2;
 		h = image.size().height / 2;
+
 		Rect rect(x, y, w, h);
 		rectangle(image, rect, Scalar(255, 0, 0));
 		rectimg = image(rect);
-		// 캐니 알고리즘 적용
-		Mat contours;
-		Canny(rectimg, contours, 125, 350);
+
+		Mat contours, mask1, mask2, mask3, test, hsv;
+		Scalar lower_white = Scalar(180, 180, 180); //흰색 차선 (RGB)
+		Scalar upper_white = Scalar(255, 255, 255);
+		inRange(rectimg, lower_white, upper_white, mask1);
+		
+		cvtColor(rectimg, hsv, COLOR_BGR2HSV); //hsv변경 (노랑잡기)
+		Scalar lower_yellow = Scalar(10, 100, 100); //노란색 차선 (HSV)
+		Scalar upper_yellow = Scalar(40, 255, 255);
+		inRange(hsv, lower_yellow, upper_yellow, mask2);
+
+		Scalar lower_blue = Scalar(110, 40, 70); //파란색 차선 (HSV)
+		Scalar upper_blue = Scalar(150, 100, 130);
+		inRange(hsv, lower_blue, upper_blue, mask3);
+
+		addWeighted(mask1, 1.0, mask2, 1.0, 0.0, contours);
+
+		addWeighted(contours, 1.0, mask3, 1.0, 0.0, contours);
+
+		imshow("t", contours);
+
+		//bitwise_and(image, hsv, test, mask3);
+
+		//imshow("img", image);
+		//imshow("blue_image", test);
+		//imshow("blue_mask", mask3);
+
+		Canny(image, contours, 125, 350); 		// 캐니 알고리즘 적용
 		// 선 감지 위한 허프 변환
 		vector<Vec2f> lines;
 		HoughLines(contours, lines, 1, PI / 180, 400); //  단계별 크기, 투표(vote) 최대 개수, 수에 따른 변화 관찰 필요 //400으로 바꿈~~
-													  
+
 		// 선 그리기
 		Mat result(contours.rows, contours.cols, CV_8U, Scalar(255));
 		cout << "Lines detected: " << lines.size() << endl;		// 선 벡터를 반복해 선 그리기
@@ -80,7 +104,9 @@ int main(int argc, char** argv)
 		}
 		namedWindow("Detected Lines with Hough");
 		imshow("Detected Lines with Hough", image);
-		// waitKey(0);
+		if (waitKey(1) == 27) {
+			break;
+		}
 	}
 	return 0;
 }
